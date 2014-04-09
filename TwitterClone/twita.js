ab53cf2a542df0c0b2425d8ebd2fab39e9961847
Mahopa <mahopa@gmail.com>
2014-04-09 11:01:58 -0500

    inital set up - LW

---
+++ TwitterClone/twita.js
@@ -0,0 +1,14 @@
+﻿var myApp = {};
+
+myApp.profile = function (name, pic, bio) {
+    this.name = name;
+    this.pic = pic;
+    this.bio = bio;
+};
+
+myApp.message = function (userName, text) {
+    this.userName = userName;
+    this.text = text;
+    this.time = Date.now();
+};
+

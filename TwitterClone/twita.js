3c426b3e78c495006b1cd03c8322b6e15dde91ac
Mahopa <mahopa@gmail.com>
2014-04-09 12:11:04 -0500

    Added message and database - dr

--- TwitterClone/twita.js
+++ TwitterClone/twita.js
@@ -1,4 +1,5 @@
 ﻿var myApp = {};
+myApp.url = "https://twita.firebaseio.com/";
 
 myApp.profile = function (name, pic, bio) {
     this.name = name;
@@ -12,3 +13,29 @@ myApp.message = function (userName, text) {
     this.time = Date.now();
 };
 
+myApp.createMessage = function () {
+    var messageText = document.getElementById("text").value;
+    var message = new myApp.message(myApp.userName, messageText);
+    myApp.postMessage(message);
+    alert(JSON.stringify(message) + " " + messageText);
+}
+
+myApp.postMessage = function (data) {
+    var request = new XMLHttpRequest();
+    request.open("Post", myApp.url+".json", true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            console.log();
+        } else {
+            console.log(this.response);
+        }
+    }
+
+    request.onerror = function () {
+        console.log("ERRRRRRRR");
+    }
+    request.send(JSON.stringify(data));
+}
+
+
+myApp.userName = "Donald";

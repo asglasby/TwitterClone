9971a4821be91f1a7a41870532ae2dd9dd84eeec
dannyrodriguez <daniel.rodriguez36@yahoo.com>
2014-04-14 13:10:09 -0500

    Initial Commit

--- TwitterClone/twita.js
+++ TwitterClone/twita.js
@@ -1,12 +1,18 @@
-﻿/// <reference path="index.html" />
-var myApp = {};
-myApp.url = "https://twita.firebaseio.com/";
+﻿
 
-myApp.profile = function (name, pic, bio) {
-    this.name = name;
+/// <reference path="index.html" />
+var myApp = {};
+myApp.db = "https://twittaclone.firebaseio.com/";
+
+myApp.profile = function (userName, pic, bio, url, friendsArray) {
+    this.userName = userName;
     this.pic = pic;
     this.bio = bio;
+    this.url = url;
+    this.friendsArray = friendsArray;  
 };
+
+
 
 myApp.message = function (userName, text) {
     this.userName = userName;
@@ -14,46 +20,45 @@ myApp.message = function (userName, text) {
     this.time = Date.now();
 };
 
+myApp.mockProfile = function () {
+    userName = "Daniel Rodriguez";
+    pic = "";
+    bio = "I'm Awesome!";
+    url = "https://twittaclone.firebaseio.com/";
+    friendsList = []
+}//Mock profile to debug will erase later.  
+
 myApp.createMessage = function () {
     var messageText = document.getElementById("text").value;
     var message = new myApp.message(myApp.userName, messageText);
-    myApp.post(message, "tweets/");
-    alert(JSON.stringify(message) + " " + messageText);
+    
+    myApp.AJAX("POST", "text" , message, null);
+    myApp.writeTable();
+    var messageText = document.getElementById("text").value = "";
+
 }
 
-myApp.post = function (data, url) {
-    var request = new XMLHttpRequest();
-    if (!url) {
-        request.open("Post", myApp.url + ".json", true);
-    }
-    else {
-        request.open("Post", myApp.url + url + ".json", true);
-    }
-    request.onload = function () {
-        if (this.status >= 200 && this.status < 400) {
-            console.log();
-            myApp.get(url);
-        } else {
-            console.log(this.response);
-        }
+
+/*myApp.post = function (data, url) {
+   myApp.AJAX("POST", )
     }
 
     request.onerror = function () {
         console.log("ERRRRRRRR");
     }
     request.send(JSON.stringify(data));
-}
+}*/
 
 myApp.get = function (url) {
 
     var request = new XMLHttpRequest();
     if (!url) {
-        request.open("GET", myApp.url + ".json", true);
+        request.open("GET", myApp.db + ".json", true);
         alert("Please input the valid url");
         return undefined;
     }
     else {
-        request.open("GET", myApp.url + url + ".json", true);
+        request.open("GET", myApp.db + url + ".json", true);
     }
     request.onload = function () {
         if (this.status >= 200 && this.status < 400) {
@@ -80,7 +85,6 @@ myApp.get = function (url) {
 myApp.tweets = [];
 
 
-myApp.userName = "Donald";
 myApp.writeTable = function (array) {
     document.getElementById("displayTweets").innerHTML = "";
     for (var i in array) {
@@ -123,4 +127,40 @@ myApp.delete = function (key, url) {
     }
     request.send();
 }
+
+myApp.URLMaker = function (array) {
+    var url = myApp.db;
+    if(array.length){
+        for(var x in array){
+            url =+ array[x] + "/";
+        }
+    }
+    url + ".json";
+    return url;
+}
+
+myApp.AJAX = function (method, urlArray, data, callback) {
+    var request = new XMLHttpRequest();
+    var url = myApp.URL(urlArray);
+    request.open(method, url, true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            var returnData = JSON.parse.(this.response);
+            if(callback){
+                callback(returnData);
+            }
+        }else
+            {console.log("Error: "+ this.response);
+        }
+
+    }
+    request.onerror = function (){
+        console.log("ERRRRR");
+    }
+    if(data){
+        request.send(JSON.stringify(data));
+    }else{
+        request.send();
+    }
+}//Master AJAX call
 myApp.get("tweets/");
